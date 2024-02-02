4753e41e1005731ae682a0b52b7f477d1ce3b7ab
Abdelouafi TAHIRI <tahiriabdelouafi@gmail.com>
2024-02-02 17:02:24 +0100

    Initialiser une application (myapp) angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/general/home/home.component';
+import { NotFoundComponent } from './pages/general/not-found/not-found.component';
+import { AboutComponent } from './pages/general/about/about.component';
+import { ContactComponent } from './pages/general/contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
+import * as fr from '@angular/common/locales/fr'
+import { registerLocaleData } from '@angular/common';
+import { UcfirstPipe } from './pipes/ucfirst.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    AboutComponent,
+    ContactComponent,
+    UcfirstPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'fr_FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule
+{
+  constructor() {
+    registerLocaleData(fr.default)
+  }
+}
